73c43bcffa61371f9d730201a91790fbecc7bd20
Stackwyre <stackwyre@gmail.com>
2026-05-10 07:37:48 -0500

    Address review feedback: update src/bounty/dto/update-bounty.dto.ts

--- src/bounty/dto/update-bounty.dto.ts
+++ src/bounty/dto/update-bounty.dto.ts
@@ -1,7 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBountyDto } from './create-bounty.dto';
-import { OmitType } from '@nestjs/mapped-types';
 
-export class UpdateBountyDto extends PartialType(
-  OmitType(CreateBountyDto, ['createdBy'] as const),
-) {}
+export class UpdateBountyDto extends PartialType(CreateBountyDto) {}
