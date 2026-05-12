1d55e6d06cb88628c24e29b7a8b10c0aa712395d
Stackwyre <stackwyre@gmail.com>
2026-05-11 19:46:58 -0500

    Address review feedback: update src/bounty/bounty.entity.ts

--- src/bounty/bounty.entity.ts
+++ src/bounty/bounty.entity.ts
@@ -34,11 +34,11 @@ export class Bounty {
   })
   status: BountyStatus;
 
-  @Column({ nullable: true })
-  creatorId: string;
+  @Column()
+  createdBy: string;
 
   @Column({ nullable: true })
-  claimedBy: string;
+  claimedBy?: string;
 
   @CreateDateColumn()
   createdAt: Date;
